d69ec68881fe1b8688cbf8e322ce1a5b829a4f7f
v0agent <it+v0agent@vercel.com>
2026-06-24 00:33:13 +0000

    feat: remove MyCourses and Announcements components from StudentDashboardShell
    Co-authored-by: SAYED <241502834+sayouuuuud@users.noreply.github.com>

--- components/student/student-dashboard-shell.tsx
+++ components/student/student-dashboard-shell.tsx
@@ -2,11 +2,8 @@ import { StudentLayout } from './student-layout'
 import { StudentWelcome } from './student-welcome'
 import { StudentStats } from './student-stats'
 import { ContinueLearning } from './continue-learning'
-import { MyCourses } from './my-courses'
 import { UpcomingSchedule } from './upcoming-schedule'
-import { RecentGrades } from './recent-grades'
 import { LearningActivityChart } from './learning-activity-chart'
-import { Announcements } from './announcements'
 
 export function StudentDashboardShell() {
   return (
@@ -24,19 +21,7 @@ export function StudentDashboardShell() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-4 xl:grid-cols-3">
-          <div className="xl:col-span-2">
-            <LearningActivityChart />
-          </div>
-          <div className="xl:col-span-1">
-            <RecentGrades />
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-          <MyCourses />
-          <Announcements />
-        </div>
+        <LearningActivityChart />
       </div>
     </StudentLayout>
   )
